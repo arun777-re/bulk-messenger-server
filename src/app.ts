b562daffa731b5487d6a53ff30e06e0f11e1c4c5
Arun <arunjamdagni2@gmail.com>
2025-12-05 23:49:10 +0530

    fix: cors bug fix

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,24 @@
-import dotenv from 'dotenv';
-dotenv.config();
+
 
 import express from 'express';
-import fs from 'fs'
 import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+import fs from 'fs';
 import path from 'path';
 import csvRoutes from './routes/csv.route';
 import authRoutes from './routes/auth';
 import messageRoutes from './routes/sendMessage';
 import { connectDB } from './middleware/mongo';
 
+
 const app = express();
+app.use(cors({origin:["https://wtsapp-bulk-message-sender.vercel.app", "http://localhost:3000"],
+  methods:["GET","POST","PUT","DELETE","PATCH"],
+  credentials:true,
+}));
+
+app.options("*",cors());
 
 // db connection 
 connectDB();
@@ -20,17 +28,11 @@ if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir, { recursive: true });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({origin:["https://wtsapp-bulk-message-sender.vercel.app", "http://localhost:3000"]
-}));
+
 
 // Static files
 app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 
-
-app.use((req, res, next) => {
-  if (req.method === "GET") return next(); 
-  return express.json()(req, res, next);
-});
 
 // Routes
 app.use('/api/csv', csvRoutes);
